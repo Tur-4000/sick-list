bbfd406371dca7e61f7f27c617ab155464117b5c
Turbanov <tur.4000@gmail.com>
2018-07-27 23:20:18 +0300

    Форма логина

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,18 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', title='Глвная')
+    return render_template('index.html', title='Главная')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Запрошена авторизация для пользователя {}, Запомнить меня={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Войти', form=form)
