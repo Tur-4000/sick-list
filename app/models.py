25f2da7f49c7edb72d7e0f156948b848ed8f31a1
Turbanov Valeriy <tur.4000@gmail.com>
2018-08-03 14:04:08 +0300

    Начато создание модели данных (Doctors)

--- app/models.py
+++ app/models.py
@@ -1,3 +1,16 @@
-from app import app, db, login
+from app import app, db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+
+class Doctors(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username =db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    last_name = db.Column(db.String(64))
+    first_name = db.Column(db.String(64))
+    middle_name = db.Column(db.String(64))
+    job_title = db.Column(db.String(254))
+
+    def __repr__(self):
+        return '<Доктор {}>'.format(self.username)
