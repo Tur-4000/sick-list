bc84c9e442987b0fb67443eac49dd5aae2952a2e
Turbanov Valeriy <tur.4000@gmail.com>
2018-08-03 13:08:47 +0300

    Добавлен Bootstrap

--- app/routes.py
+++ app/routes.py
@@ -4,4 +4,4 @@ from app import app
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', title='Глвная')
+    return render_template('index.html', title='Главная')
