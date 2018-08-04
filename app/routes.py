03f9e12496e736e39df678c7b88e7e137d9da7d8
Turbanov <tur.4000@gmail.com>
2018-08-05 00:18:49 +0300

    Добавлена фиксация времени последней активности пользователя

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,14 @@ from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import Doctors
 from werkzeug.urls import url_parse
+from datetime import datetime
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_visit = datetime.utcnow()
+        db.session.commit()
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
