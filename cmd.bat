c0497004b99f4d0c2ac5667d39eac8d741603260
Turbanov Valeriy <tur.4000@gmail.com>
2018-11-19 15:01:10 +0200

    fix migration bug
    add create column diagnoses_id in lists table

--- cmd.bat
+++ cmd.bat
@@ -1,4 +1,6 @@
 set FLASK_APP=sicklist.py
 set FLASK_DEBUG=1
-set DEV_DATABASE_URL=mysql+pymysql://sicklistuser:111@localhost:3306/devsicklist
+set FLASK_CONFIG=development
+REM set DEV_DATABASE_URL=mysql+pymysql://sicklistuser:111@localhost:3306/devsicklist
+set DEV_DATABASE_URL=postgresql://sicklistuser:111@localhost/devsicklist
 start cmd.exe /K .\venv\Scripts\activate.bat
